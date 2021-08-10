6ad9cabc4797358741939cd92268c5753ddf5e83
Bruno Souto <bcovies@gmail.com>
2021-08-10 20:21:44 +0000

    Adicionado Request(php) Response(node)

--- node/server.js
+++ node/server.js
@@ -12,5 +12,13 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+
+app.get('/sms', function (req, res) {
+    const teste = req.query.param1
+    console.log(teste)
+    res.set('Content-Type', 'text/plain')
+    res.send(`You sent: ${teste} to Express\n`)
+})
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
--- php/conexao.php
+++ php/conexao.php
@@ -1,26 +1,27 @@
 <?php
+include("enviaGET.php");
 // Create a cURL handle
 $ch = curl_init('http://node:5000');
+$cores = array('azul','vermelho','branco','preto','roxo','laranja','rosa','lilas','verde','amarelo','cinza');
 $execucoes = 1;
-while ($execucoes <= 10){
-    // Execute
-    curl_exec($ch);
+    while ($execucoes <= 10){
+        // Execute
+        curl_exec($ch);
 
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-    if($http_code != 0){
-        echo "\nResposta HTTP: $http_code";
-        if($http_code == 200){
-            echo "\tTudo certo! Começo do script de comunicação...\n";
-
+        if($http_code != 0){
+            echo "\nResposta HTTP: $http_code";
+            if($http_code == 200){
+                echo "\tTudo certo! Começo do script de comunicação...\n";
+                manda_mensagem($cores[$execucoes]);
+            }
+        }else{
+            echo "\nServidor offline --> HTTP: $http_code \n";
         }
-    }else{
-        echo "\nServidor offline --> HTTP: $http_code \n";
+        // Close handle
+        curl_close($ch);
+        $execucoes ++ ;
+        sleep(3);
     }
-    // Close handle
-    curl_close($ch);
-    $execucoes ++ ;
-
-}
-
 ?>
