8314cdede3b33b9521c8fef2c0d3210b69c61ce3
Bruno Souto <bcovies@gmail.com>
2021-10-18 16:01:13 -0300

    Adicionado final

---
+++ mono2/vm_docker/swarm_ansible.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script para provisionamento + inicialização das máquina virtuais por meio do vagrant.
+
+# DELCARAÇÃO VARIÁVEIS
+SCRIPT_NOME=docker_swarm
+
+data_de_hoje(){
+    date +%Y-%m-%d
+} 
+
+segundo_agora(){
+    date +%H:%M:%S
+}
+LOCALIZACAO_LOGS=./logs/$SCRIPT_NOME.log
+
+echo "Começo do scrpit $SCRIPT_NOME em: $(data_de_hoje) às $(segundo_agora)." 2>&1>> $LOCALIZACAO_LOGS 
+
+echo "Inicialização ANSIBLE docker API  em: $(data_de_hoje) às $(segundo_agora)." 2>&1>> $LOCALIZACAO_LOGS 
+
+sleep 5
+
+ansible-playbook /home/dev/git/monografia/mono2/vm_docker/ansible/docker_mgr.yml -i /home/dev/git/monografia/mono2/vm_docker/ansible/hosts
+
+sleep 5
+
+ansible-playbook /home/dev/git/monografia/mono2/vm_docker/ansible/docker_wkr01.yml -i /home/dev/git/monografia/mono2/vm_docker/ansible/hosts 
+
+sleep 5
+
+ansible-playbook /home/dev/git/monografia/mono2/vm_docker/ansible/docker_wkr02.yml -i /home/dev/git/monografia/mono2/vm_docker/ansible/hosts 
+
+
---
+++ mono2/vm_docker/swarm_vagrant.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script para provisionamento + inicialização das máquina virtuais por meio do vagrant.
+
+# DELCARAÇÃO VARIÁVEIS
+SCRIPT_NOME=docker_swarm
+
+data_de_hoje(){
+    date +%Y-%m-%d
+} 
+
+segundo_agora(){
+    date +%H:%M:%S
+}
+
+LOCALIZACAO_LOGS=./logs/$SCRIPT_NOME.log
+LOCALIZACAO_HOSTS="/home/dev/.ssh/known_hosts"
+
+echo "Começo do scrpit $SCRIPT_NOME em: $(data_de_hoje) às $(segundo_agora)." 2>&1>> $LOCALIZACAO_LOGS 
+
+echo "Remoção de arquivos de HOST de SSH em: $LOCALIZACAO_HOSTS:  $(data_de_hoje) às $(segundo_agora)." 2>&1>> $LOCALIZACAO_LOGS 
+rm -f $LOCALIZACAO_HOSTS
+
+sleep 1
+
+echo "Inicialização VAGRANT da máquina virtual docker_manager em: $(data_de_hoje) às $(segundo_agora)." 2>&1>> $LOCALIZACAO_LOGS 
+vagrant up docker_manager docker_worker_1 docker_worker_2 >> $LOCALIZACAO_LOGS
+
+echo "Máquinas docker_manager docker_worker_1  docker_worker_2 inicializadas, tempo de espera de  para normalização do uso de processamento: $(data_de_hoje) às $(segundo_agora)." 2>&1>> $LOCALIZACAO_LOGS 
+sleep 1
+
+ansible-playbook /home/dev/git/monografia/mono2/vm_docker/ansible/ssh.yml -i /home/dev/git/monografia/mono2/vm_docker/ansible/hosts
